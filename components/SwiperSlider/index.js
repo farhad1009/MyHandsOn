b5ef32c16a0f89b74332758945bec48ae228bb20
farhad1009 <farhad1009@gmail.com>
2021-10-26 16:11:00 +0600

    Swiper Slide edited

--- components/SwiperSlider/index.js
+++ components/SwiperSlider/index.js
@@ -1,57 +1,49 @@
 import React from 'react'
+import Image from 'next/image'
 import { Swiper, SwiperSlide } from 'swiper/react';
 // Import Swiper styles
-import { Navigation, Pagination, Scrollbar, A11y, } from 'swiper';
+import SwiperCore, { Navigation, Pagination } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
+
+SwiperCore.use([Navigation, Pagination]);
 
 export default function Index() {
+    
     return (
         <>
             <Swiper
-                // install Swiper modules
-                modules={[Navigation, Pagination, Scrollbar, A11y,]}
                 spaceBetween={2}
                 slidesPerView={1}
                 autoplaydisableoninteraction={"false"}
                 loop={true}
                 pagination={{ clickable: true }}
-                navigation={{ clickable: true }}
+                className="custom-class"
+                    navigation={{
+                        prevEl: ".custom-prev",
+                        nextEl: ".custom-next",
+                    }}
                 scrollbar={{ draggable: true }}
                 onSwiper={(swiper) => console.log(swiper)}
                 onSlideChange={() => console.log('slide change')}
             >
-                <SwiperSlide className="">
-                    <div>SLIDE 1
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide className="">
-                    <div>SLIDE 2
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide className="">
-                    <div>SLIDE 3
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide className="">
-                    <div>SLIDE 4
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide className="">
-                    <div>SLIDE 5
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide className="">
-                    <div>SLIDE 6
-                    </div>
-                </SwiperSlide>
-                <SwiperSlide className="">
-                    <div>SLIDE
-                    </div>
-                </SwiperSlide>
+                
+                    <SwiperSlide>
+                        <h1>Slider 1</h1>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <h1>Slider 2</h1>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <h1>Slider 3</h1>
+                    </SwiperSlide>
+               
             </Swiper>
+            <div>
+                <div className="custom-prev">Pre</div>
+                <div className="custom-next">Next</div>
+            </div>
         </>
     )
 }
